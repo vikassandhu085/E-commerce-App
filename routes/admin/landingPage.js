39eb71abb728902def65e8d080a6db0a34c8b529
vikas <i.vikas.sandhu@generalaeronautics.com>
2023-01-27 14:32:16 +0530

    changes

--- routes/admin/landingPage.js
+++ routes/admin/landingPage.js
@@ -3,7 +3,7 @@ const express = require("express")
 const router = express.Router()
 
 router.get("/", function(req,res) {
-    res.render("homePage.ejs");
+    res.render("homepage.ejs");
 })
 
 module.exports = router;
